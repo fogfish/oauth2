43580e61527c81d687a8e7a70ba97ba15ba9e913
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-12-12 23:16:26 +0200

    clean-up all resource after stack removal

--- cloud/src/storage.ts
+++ cloud/src/storage.ts
@@ -2,11 +2,12 @@ import * as ddb from '@aws-cdk/aws-dynamodb'
 import * as cdk from '@aws-cdk/core'
 import * as pure from 'aws-cdk-pure'
 
-export const DDB = (): pure.IPure<ddb.Table> => {
+export const DDB = (vsn: string): pure.IPure<ddb.Table> => {
   const iaac = pure.iaac(ddb.Table)
   const AuthPubKey = (): ddb.TableProps => ({
     partitionKey: {type: ddb.AttributeType.STRING, name: 'prefix'},
     readCapacity: 1,
+    removalPolicy: vsn.startsWith('pr') ? cdk.RemovalPolicy.DESTROY : cdk.RemovalPolicy.RETAIN,
     sortKey: {type: ddb.AttributeType.STRING, name: 'suffix'},
     tableName: `${cdk.Aws.STACK_NAME}-pubkey`,
     writeCapacity: 1,
